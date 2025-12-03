287e8353ee9066ccf15e764fa67a9e0fa453a7c1
malakelabdellaoui44-a11y <malakelabdellaoui44@gmail.com>
2025-12-03 20:20:22 +0100

    Update gestion_niveau.h

--- include/gestion_niveau.h
+++ include/gestion_niveau.h
@@ -1,14 +1,16 @@
 #ifndef GESTION_NIVEAU_H
 #define GESTION_NIVEAU_H
 
-#include "common.h"     // NB_LIGNES, NB_COLONNES, NB_TYPES_ITEM, Contrat
+#include "common.h"   // NB_LIGNES, NB_COLONNES, Contrat
 #include <stdio.h>
 #include <stdlib.h>
 
-// --------- PROTOTYPES DU MODULE 4 ---------
+// -------- PROTOTYPES DU MODULE 4 --------
 
+// 1. charger un contrat depuis le fichier
 void chargerContrat(int niveau, Contrat *c);
 
+// 2. initialiser le niveau
 void initNiveau(int niveau,
                 int grille[NB_LIGNES][NB_COLONNES],
                 Contrat *c,
@@ -16,13 +18,21 @@ void initNiveau(int niveau,
                 int *tempsRestant,
                 int *score);
 
-void majContratApresSuppression(int typeFruit, int nb, int *score,
-                                Contrat *c, int *coupsRestants);
+// 3. mettre à jour le contrat après suppression
+void majContratApresSuppression(int typeFruit,
+                                int nb,
+                                Contrat *c,
+                                int *coupsRestants);
 
-int testSuccesContrat(Contrat c);
+// 4. tester si le contrat est réussi
+int testSuccesContrat(Contrat *c);
 
-int testEchecNiveau(int coupsRestants, int tempsRestant, Contrat c);
+// 5. tester si le niveau est échoué
+int testEchecNiveau(int coupsRestants,
+                    int tempsRestant,
+                    Contrat c);
 
+// 6. jouer un niveau entier
 void jouerNiveau(int niveau, int *vies);
 
 #endif
