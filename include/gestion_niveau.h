1ae721f0ee0930d45a1395827a8044353624471c
malakelabdellaoui44-a11y <malakelabdellaoui44@gmail.com>
2025-12-03 19:30:31 +0100

    module 4 header

--- include/gestion_niveau.h
+++ include/gestion_niveau.h
@@ -1,34 +1,28 @@
-// include/gestion_niveau.h
-
 #ifndef GESTION_NIVEAU_H
 #define GESTION_NIVEAU_H
 
-// --- 1. Définitions et Structures ---
-// Constantes définies dans votre analyse [cite: 476, 477]
-#define LIGNES 25
-#define COLONNES 45
-#define NB_TYPES_ITEMS 5 // 5 types d'items de base (bonbons/fruits)
+#include "common.h"     // NB_LIGNES, NB_COLONNES, NB_TYPES_ITEM, Contrat
+#include <stdio.h>
+#include <stdlib.h>
 
-// Structure pour stocker les objectifs du contrat du niveau [cite: 140, 142, 144, 147]
-typedef struct {
-    int objectifs[NB_TYPES_ITEMS]; // Quantité de chaque item à éliminer (contrat[5])
-    int coupsRestants;             // Nombre de coups maximum [cite: 145]
-    int tempsRestant;              // Timer du niveau (en secondes) [cite: 142]
-} Contrat;
+// --------- PROTOTYPES DU MODULE 4 ---------
 
-
-// --- 2. Prototypes du Module 4 (Gestion Niveau) ---
-// Fonctions d'initialisation et de jeu
 void chargerContrat(int niveau, Contrat *c);
-void initNiveau(int niveau, int grille[LIGNES][COLONNES], Contrat *c);
+
+void initNiveau(int niveau,
+                int grille[NB_LIGNES][NB_COLONNES],
+                Contrat *c,
+                int *coupsRestants,
+                int *tempsRestant,
+                int *score);
+
+void majContratApresSuppression(int typeFruit, int nb, int *score,
+                                Contrat *c, int *coupsRestants);
+
+int testSuccesContrat(Contrat c);
+
+int testEchecNiveau(int coupsRestants, int tempsRestant, Contrat c);
+
 void jouerNiveau(int niveau, int *vies);
 
-// Fonctions de logique du niveau
-void majContratApresSuppression(int typeFruit, int nb, int *score, Contrat *c);
-
-// Fonctions de test (déjà détaillées aux étapes 3 et 4)
-int testSuccesContrat(Contrat c);
-int testEchecNiveau (int coups, int temps, Contrat c);
-
-
-#endif // GESTION_NIVEAU_H
+#endif
